ab605a5aca8457bbca5c19f42e2df613c189363e
lucas <lucasmio98@gmail.com>
2025-04-06 21:44:39 -0300

    advancing through edit page

--- src/components/Pages/editEntry.jsx
+++ src/components/Pages/editEntry.jsx
@@ -1,7 +1,36 @@
+import { NavLink } from "react-router-dom";
+
 export default function Edit(){
     return (
     <>
-        <h1>Editing an anime</h1>
+        <div className="sectionEdit">
+            <h1 className="title">Edit an anime</h1>
+            <div className="dataToModify">
+                <div className="formGroup">  
+                    <label htmlFor="titleInp">Title: </label>
+                    <input type="text" id="titleInp" readOnly={true}/>
+                </div>
+                <div className="formGroup">
+                    <label htmlFor="seasonsInp">Number of anime seasons: </label>
+                    <input type="text" id="seasonsInp"/>
+                </div>
+                <div className="formGroup">
+                    <label htmlFor="genresInp">Genres: </label>
+                    <input type="text" id="genresInp"/>
+                </div>
+                <div className="formGroup">
+                    <label htmlFor="image">Front page: </label>
+                    <input type="file" id="imageInp" accept="image/png, image/jpeg" />
+                </div>
+            </div>
+            <div className="btns-edit-container">
+                <button type="button" className="btns-edit btn-confirm">Confirm</button>
+                <button type="button" className="btns-edit btn-discard">Discard</button>
+                <button type="button" className="btns-edit btn-go">
+                    <NavLink to="/" className="go-back-link link">Go back</NavLink>
+                </button>
+            </div>
+        </div>
     </>
 )
 }
