711ecd0733ba889ff7fa122568ac1be0d1459a2b
lucas <lucasmio98@gmail.com>
2025-06-25 00:17:34 -0300

    borrar campo genres y agregar useEffect llamando a la api en el componente main

--- src/Pages/main.jsx
+++ src/Pages/main.jsx
@@ -1,6 +1,18 @@
 import AnimesContainer from "../components/AnimesContainer"
 import { NavLink } from "react-router-dom"
+import { useState, useEffect } from "react"
 export default function MainPage(){
+    const [dataAnimes,setData] = useState(null)
+    const url = "http://localhost:3000/animes/series"
+    const getData = async ()=>{
+        const response = await fetch(url)
+        const data = await response.json()
+        setData(data)
+    }
+    useEffect(()=>{
+        getData()
+    },[])
+    console.log("data:",dataAnimes)
     return (      
         <>
             <h1 className="title">My fav animes!</h1>
@@ -9,7 +21,7 @@ export default function MainPage(){
                     <button id="add-anime" className="btn btn-add"><NavLink to="/newEntry">New Anime</NavLink></button>
                 </div>
                 <hr></hr>
-                <AnimesContainer />
+                <AnimesContainer/>
             </main>
         </>
     )
--- src/components/AnimeBox/index.jsx
+++ src/components/AnimeBox/index.jsx
@@ -5,7 +5,7 @@ function deleteAnime (title){
     const indexAnime = animes.findIndex((elem)=> {return elem.title===title})
     animes.splice(indexAnime,1)
 }
-export default function Container({imgSrc, title, alt, seasons, genres,renderFunction}){
+export default function Container({imgSrc, title, alt, seasons,renderFunction}){
     function renderMainContainer(){
         renderFunction(Math.random())
     }
@@ -14,8 +14,7 @@ export default function Container({imgSrc, title, alt, seasons, genres,renderFun
             <img src={imgSrc} alt={alt}></img>
             <p className="anime-title">Title: {title}.</p>
             <p className="n-seasons">Seasons: {seasons}.</p>
-            <p className="genre">Genres: {genres.join(", ")}.</p>
-            <button type="button" className="btn-anime btn-edit"><NavLink to="/edit" className="edit link" state={{title: title, seasons: seasons, genres: genres}}>Edit anime</NavLink></button>
+            <button type="button" className="btn-anime btn-edit"><NavLink to="/edit" className="edit link" state={{title: title, seasons: seasons}}>Edit anime</NavLink></button>
             <button type="button" className="btn-anime btn-delete" onClick={()=> 
                 {
                     deleteAnime(title)
