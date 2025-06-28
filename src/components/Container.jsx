b96758d6e4950835bcf5a1f84999da81300756bb
lucas <lucasmio98@gmail.com>
2025-06-28 14:06:10 -0300

    borrar animes parece ok

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,13 @@
 import { NavLink } from 'react-router-dom';
 
-export default function Container({imgSrc, title, alt, seasons,id}){
-    const deleteAnime = (title)=>{return title}
+export default function Container({imgSrc, title, alt, seasons,id,onDelete}){
+    const deleteAnime =  async (id)=>{
+        const url_api=`http://localhost:3000/animes/series`
+        const res = await fetch(`${url_api}/${id}`,{method: "DELETE"});
+        const data = await res.json();
+        onDelete(true)
+        console.log(data)
+    }
     return (
         <div className="anime-box">
             <img src={imgSrc} alt={alt}></img>
@@ -10,7 +16,7 @@ export default function Container({imgSrc, title, alt, seasons,id}){
             <button type="button" className="btn-anime btn-edit"><NavLink to="/edit" className="edit link" state={{idState: id}}>Edit anime</NavLink></button>
             <button type="button" className="btn-anime btn-delete" onClick={()=> 
                 {
-                    deleteAnime(title);
+                    deleteAnime(id);
                 }          
             }>Delete anime</button>
         </div> 
