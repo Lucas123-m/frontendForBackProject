0a46034e95aafd36dc5064490dd70e9bfd174398
lucas <lucasmio98@gmail.com>
2025-07-02 03:07:49 -0300

    vuelvo a simplificar add images

--- src/Pages/manageImages.jsx
+++ src/Pages/manageImages.jsx
@@ -1,21 +1,7 @@
 import AddImage from "../components/forms/addImage"
-import { useState } from "react";
 export default function ManageImages(){
-    const [form,setForm] = useState("add") 
-    const options = [{value:"add",label:"add image"},{value: "update",label:"update image"},{value:"delete",label:"delete image"}]
-    const handleFormImg = (event)=>{
-        setForm(event.target.value)
-        console.log(form)
-    }
     return (
     <>
-        <h1>Manage images</h1>
-        <div className="selectManageImg">
-            <label htmlFor="watchStatusInp">Watch status: </label>
-            <select value={form} name="Watch Status" id="watchStatusInp" onChange={handleFormImg} required>
-                {options.map(({value,label})=>{return <option key={value} value={value}>{label}</option>})}  
-            </select>
-        </div>
         <AddImage></AddImage>
     </>
 )
