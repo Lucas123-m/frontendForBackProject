9833bee05451da8275d699d7a77333f5d24747d2
lucas <lucasmio98@gmail.com>
2025-07-05 11:52:23 -0300

    agrego pagina para borrar imagenes

--- src/Pages/manageImages.jsx
+++ src/Pages/manageImages.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react"
 import AddImage from "../components/forms/addImage"
+import DeleteImage from "../components/forms/deleteImage"
 export default function ManageImages(){
+    const [action,setAction]=useState("add")
+
+    const handleActionSelected = (e)=>{
+        setAction(e.target.value)
+    }
+    const manage = ()=>{
+        const selectTag = document.getElementById("selectActionImg")
+            if (action === "add"){
+                return <AddImage></AddImage>
+            } else if (selectTag.value === "delete"){
+                return <DeleteImage></DeleteImage>
+            } else {
+                return <div><h1>Accion no encontrada</h1></div>
+            }
+        }
+    const options = [{value: "add",label: "Add an image"},{value: "delete",label: "Delete an image"}]
     return (
     <>
-        <AddImage></AddImage>
+        <div>
+            <select value={action} name="selectAction" id="selectActionImg" onChange={handleActionSelected} required>
+                {options.map(({value,label})=>{return <option key={value} value={value}>{label}</option>})}  
+            </select>
+        </div>
+        {manage()}
     </>
-)
+    )
 }
---
+++ src/components/forms/deleteImage.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react"
+import { NavLink } from "react-router-dom"
+export default function DeleteImage(){
+    const [urlImage,setUrlImage] = useState('')
+    const [idImage,setIdImage] = useState(0)
+    const [imgName,setImgName] = useState("N/A")
+    const [images,setImages] = useState([])
+    useEffect(()=>{
+        const getImages = async()=>{
+            const res = await fetch("http://localhost:3000/animes/images")
+            const data = await res.json()
+            data.push({id:0,name:"N/A",url: ""})
+            setImages(data)
+
+            return data
+        } 
+        getImages()
+    },[])
+
+    const handleimageName = (event)=>{
+        setIdImage(parseInt(event.target.value))
+        setImgName(event.target.selectedOptions[0].text)
+        setUrlImage(event.target.selectedOptions[0].id)
+        console.log(idImage,imgName,urlImage)
+    }
+
+    const handleSubmit= async (event)=>{
+        event.preventDefault()
+        const res = await fetch(`http://localhost:3000/animes/images/${idImage}`,{method: "DELETE"})
+        const data = await res.json() 
+        console.log(data)
+    }
+    return(
+        <>
+            <div className="sectionAdd">
+                <h1 className="title">Delete an image</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="dataToModify">
+                        <div className="formGroup">
+                            <label htmlFor="imageModif">Img: </label>
+                            <select value={idImage} name="image selection" id="imageModif" onChange={handleimageName} required>
+                                {images.map(({id,url,name})=>{return <option id={url} key={id} value={id}>{name}</option>})}
+                            </select>
+                        </div>
+                        <div className="formGroup">
+                            <label htmlFor="imageModif">Img selected: </label>
+                            <img src={urlImage || null} alt={imgName} />
+                        </div>
+                    </div>
+                    <div className="btns-edit-container">
+                        <button type="submit" className="btns-edit btn-confirm">Delete</button>
+                        <button type="button" className="btns-edit btn-go">
+                            <NavLink to="/" className="go-back-link link">Go back</NavLink>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </>
+    )
+}
